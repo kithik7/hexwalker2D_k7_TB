50fe3495c6918b7df580367bcc51a03c635a7677
kithik7 <keerthikesavan06@gmail.com>
2026-08-31 16:57:34 +0200

    added first comments, get more done keerthi wtf

---
+++ Hex_BluePrint/HexWalker_leg.py
@@ -0,0 +1,7 @@
+#file contains one class called HexWalker Leg
+#class method = __init__ the constructor (receives info: ID, leg label, origin point, 
+#workspace center - offset from origin to the middle leg of the reacahable zone
+#workspace radius - how large the reachable zone is
+
+#INSIDE CLASS leg, init stores each leg on the object it"self" 
+#default values: Stance Amplitude is 
